89aaed86b10ab27fbe459e5525f82424214afc29
B S K <Home@Bs-MacBook-Air.local>
2019-02-11 02:17:28 +0530

    Don't snapshot if already in progress

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -15,6 +15,10 @@ def filter_instances(project):
         instances=ec2.instances.all()
     return instances
 
+def has_pending_snapshot(volume):
+    snapshots=list(volume.snapshots.all())
+    return snapshots and snapshots[0].state=='pending'
+
 @click.group()
 def cli():
     """Shotty manages snapshots"""
@@ -93,6 +97,9 @@ def create_snapshots(project):
         i.stop()
         i.wait_until_stopped()
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print("Skipping snapshot of {0}. ". format(v.id) )
+                continue
             print("Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by Snapshotalyzer 30000")
 
